cc9b6d3a04781581e26a874511a95ee7d52769b2
sup56006 <isaackyuma56@gmail.com>
2026-01-18 22:59:01 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
+
 from flask import Flask, request, make_response, jsonify
-from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 from models import db, Message
 
@@ -10,17 +12,71 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 CORS(app)
-migrate = Migrate(app, db)
 
+migrate = Migrate(app, db)
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/')
+def home():
+    return {'message': 'Chatterbox API running'}, 200
+
+
+# -------------------- MESSAGES --------------------
+
+@app.route('/messages', methods=['GET', 'POST'])
+def messages():
+
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        return make_response(
+            jsonify([message.to_dict() for message in messages]),
+            200
+        )
+
+    elif request.method == 'POST':
+        data = request.get_json()
+
+        new_message = Message(
+            body=data.get('body'),
+            username=data.get('username')
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        return make_response(
+            jsonify(new_message.to_dict()),
+            201
+        )
+
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
+def message_by_id(id):
+    message = Message.query.filter_by(id=id).first()
+
+    if not message:
+        return make_response({'error': 'Message not found'}, 404)
+
+    if request.method == 'PATCH':
+        data = request.get_json()
+
+        if 'body' in data:
+            message.body = data['body']
+
+        db.session.commit()
+
+        return make_response(
+            jsonify(message.to_dict()),
+            200
+        )
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        return make_response({}, 204)
+
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5000, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,22 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.sql import func
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+db = SQLAlchemy()
 
-db = SQLAlchemy(metadata=metadata)
 
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(
+        db.DateTime,
+        server_default=func.now(),
+        onupdate=func.now()
+    )
+
+    serialize_rules = ('-updated_at',)
